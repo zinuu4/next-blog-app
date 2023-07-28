0e81c7c8fbf788175dc1dcc1c9c493f90498d0b2
zinuu4 <unvm7721@gmail.com>
2023-07-28 19:12:46 +0300

    Implement basic functional

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from "next";
+
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  return response.json();
+}
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  const post = await getData(id);
+
+  return {
+    title: post.title,
+  };
+}
+
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  console.log(post);
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
+}
---
+++ components/TheFooter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TheFooter = () => {
+  return <footer className="container">Created by Zinu4</footer>;
+};
+
+export default TheFooter;
